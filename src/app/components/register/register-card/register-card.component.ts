3d2709ec5200863348c464d892832c165c297d72
Prieto Bailo, León Enrique <leprieto@indra.es>
2025-06-30 00:17:12 +0200

    add initial register component.

--- src/app/components/register/register-card/register-card.component.ts
+++ src/app/components/register/register-card/register-card.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-register-card',
-  imports: [],
+  imports: [
+    FloatLabelModule,
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    CheckboxModule,
+    ButtonModule,
+  ],
   templateUrl: './register-card.component.html',
-  styleUrl: './register-card.component.scss'
+  styleUrl: './register-card.component.scss',
 })
 export class RegisterCardComponent {
-
+  username_value1: any;
+  username_value2: any;
+  checked: boolean = false;
 }
