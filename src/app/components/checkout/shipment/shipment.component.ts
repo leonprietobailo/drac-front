3cb49d94f9a7e5c2c57fc58b1d5db8eaeadb8819
Prieto Bailo, León E. <contact@leonprieto.com>
2025-08-14 13:10:55 +0200

    fix: fix carousel component issues

--- src/app/components/checkout/shipment/shipment.component.ts
+++ src/app/components/checkout/shipment/shipment.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CarouselModule } from 'primeng/carousel';
+import { Carousel, CarouselModule } from 'primeng/carousel';
 import { AddressDto, RecipientDto, ShippingResponseDto } from '../../../dto/response/checkout';
 import { DialogModule } from 'primeng/dialog';
 import { FloatLabelModule } from 'primeng/floatlabel';
@@ -18,9 +18,9 @@ import { AddressPopupComponent } from "./address-popup/address-popup.component";
 })
 export class ShipmentComponent implements OnInit, OnChanges {
 
-
-
   @Input() shippingResponse!: ShippingResponseDto;
+  @ViewChild('recipientCr') recipientCr!: Carousel;
+  @ViewChild('addressCr') addressCr!: Carousel;
 
   ShippingTypes = ShippingTypes;
   selectedType: ShippingTypes = ShippingTypes.NONE;
@@ -33,26 +33,29 @@ export class ShipmentComponent implements OnInit, OnChanges {
   value1: string | undefined;
   value2: string | undefined;
   value3: string | undefined;
-carouselWidth: number = 0;
-
+  recipientCarouselWidth: number = 0;
+  addressCarouselWidth: number = 0;
+  recipientPage: number = 0;
+  addressPage: number = 0;
   ngOnInit(): void {
-    console.log(this.shippingResponse)
-    // this.carouselWidth = 300 * this.shippingResponse;
+    this.recipientCarouselWidth = 300 * this.shippingResponse.recipients.length;
+    this.addressCarouselWidth = 300 * this.shippingResponse.addresses.length;
+
     this.responsiveOptions = [
       {
         breakpoint: '1800px',
         numVisible: 4,
-        numScroll: 1
+        numScroll: 4
       },
       {
         breakpoint: '1440px',
         numVisible: 3,
-        numScroll: 1
+        numScroll: 3
       },
       {
         breakpoint: '1100px',
         numVisible: 2,
-        numScroll: 1
+        numScroll: 2
       },
       {
         breakpoint: '767px',
@@ -62,10 +65,18 @@ carouselWidth: number = 0;
     ]
   }
 
-    ngOnChanges(changes: SimpleChanges): void {
-          if (changes['shippingResponse']) {
-           this.carouselWidth = 300 * this.shippingResponse.recipients.length;
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['shippingResponse']) {
+      this.recipientCarouselWidth = 300 * this.shippingResponse.recipients.length;
+      this.addressCarouselWidth = 300 * this.shippingResponse.addresses.length;
+      // this.recipientPage = Math.floor(this.shippingResponse.recipients.length / (this.recipientCr?.numVisible || 1))
+      // this.addressPage = Math.floor(this.shippingResponse.addresses.length / (this.addressCr?.numVisible || 1))
+
     }
+  }
+
+  trackByRecipient(index: number, r: RecipientDto) {
+    return r.id ?? index;
   }
 
   selectType(type: ShippingTypes) {
@@ -90,18 +101,27 @@ carouselWidth: number = 0;
 
   updateRecipient(recipientDto: RecipientDto) {
     this.shippingResponse.recipients = [...this.shippingResponse.recipients, recipientDto];
-    this.carouselWidth = 300 * this.shippingResponse.recipients.length;
+    this.recipientCarouselWidth = 300 * this.shippingResponse.recipients.length;
     this.selectedRecipient = recipientDto;
+
+    setTimeout(() => {
+      const visible = this.recipientCr?.numVisible || 1;
+      this.recipientPage = Math.max(0, Math.ceil(this.shippingResponse.recipients.length / visible) - 1);
+    });
+
   }
 
   updateAddress(addressDto: AddressDto) {
     this.shippingResponse.addresses = [...this.shippingResponse.addresses, addressDto];
+    this.addressCarouselWidth = 300 * this.shippingResponse.addresses.length;
     this.selectedAddress = addressDto;
+
+    setTimeout(() => {
+      const visible = this.addressCr?.numVisible || 1;
+      this.addressPage = Math.max(0, Math.ceil(this.shippingResponse.addresses.length / visible) - 1);
+    });
   }
-
-
 }
-
 
 export enum ShippingTypes {
   POINT,
